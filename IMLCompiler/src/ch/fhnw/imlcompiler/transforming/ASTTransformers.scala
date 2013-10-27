40fee0aeaa7a2b37c90c0c2b7ce3c10ddb50c8dc
bneukom <bneukom@bneukom-PC>
2013-10-27 23:37:12 +0100

    - transformation updates

--- IMLCompiler/src/ch/fhnw/imlcompiler/transforming/ASTTransformers.scala
+++ IMLCompiler/src/ch/fhnw/imlcompiler/transforming/ASTTransformers.scala
@@ -39,13 +39,48 @@ trait ASTTransformers {
 
   def containsListExpr(cmd: Cmd): Boolean = true
 
+  /*
+  	var $x1:int;
+	var $$l1:[int];
+	$x1 init := FROM;
+	$$l1 init := []
+	
+	while $x1 [< OR >] TO do
+		if WHERE(x)
+			l1 := $x1 :: l1;
+		else 
+			skip;
+		endif;
+		$x1 := $x1 [+ OR -] 1 // $x1 := $x1 - 1 falls end > start
+	endwhile
+	
+	primes init := $$l1;
+   */
+  def transformListExpr(lexpr: ListExpr, count: Int): (StoreDecl, StoreDecl, List[Cmd], StoreExpr) = {
+    val countIdent = Ident("$" + lexpr.i.value + count)
+    val listIdent = Ident("$$l" + count)
+    val counterStoreDecl = StoreDecl(Var, TypedIdent(countIdent, IntType))
+    val listStoreDecl = StoreDecl(Var, TypedIdent(listIdent, ListType(IntType)))
+    val countInitCmd = BecomesCmd(StoreExpr(countIdent, true), LiteralExpr(lexpr.from))
+    val listInitCmd = BecomesCmd(StoreExpr(listIdent, true), LiteralExpr(ListLiteral(List())))
+
+    val appenderCmd = BecomesCmd(StoreExpr(listIdent, false), DyadicExpr(StoreExpr(countIdent, false), ConcatOpr, StoreExpr(listIdent, false)))
+    val whereCmd = IfCmd(lexpr.where, appenderCmd :: Nil, SkipCmd() :: Nil)
+    val incrementer = BecomesCmd(StoreExpr(countIdent, false), DyadicExpr(StoreExpr(listIdent, false), if (lexpr.from.v < lexpr.to.v) PlusOpr else MinusOpr, LiteralExpr(IntLiteral(1))))
+
+    val whileCmd = WhileCmd(DyadicExpr(StoreExpr(countIdent, false), if (lexpr.from.v < lexpr.to.v) LT else GT, LiteralExpr(lexpr.to)), whereCmd :: incrementer :: Nil)
+    val resultExpr = StoreExpr(listIdent, false)
+    
+    return (counterStoreDecl, listStoreDecl, countInitCmd :: listInitCmd :: whileCmd :: Nil, resultExpr)
+  }
+
   def transformExpr(expr: Expr, context: Context) = {
     expr match {
       case ListExpr(ret, i, from, to, where) => {
         // TODO returns a new store expr
         // TODO how to inject the new commands/stores needed?
       }
-      case DyadicExpr(lhs, _,rhs) =>
+      case DyadicExpr(lhs, _, rhs) =>
       case MonadicExpr(rhs, _) =>
       case _ => {}
     }
