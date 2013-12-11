67717dcb4dfe7c4c54347a152f81a7e7ed0d8c8a
bneukom <bneukom@bneukom-PC>
2013-12-11 19:35:39 +0100

    - added java code for possible generation

--- IMLCompiler/src/ch/fhnw/imlcompiler/Compiler.scala
+++ IMLCompiler/src/ch/fhnw/imlcompiler/Compiler.scala
@@ -11,15 +11,6 @@ import ch.fhnw.imlcompiler.transforming.ASTTransformers
 object Compiler extends IMLParsers with SemanticAnalysis with FlowAnalysis with ASTTransformers {
 
   def main(args: Array[String]) {
-    //    val f = LiteralExpr(IntLiteral(1))
-    //    val l = List(LiteralExpr(IntLiteral(2)), LiteralExpr(ListLiteral(List())))
-    //    val ll = f :: l;
-    //    val fold = ll.take(ll.size - 1).foldRight(ll.last: Expr)((a: Expr, b: Expr) => DyadicExpr(a, ConcatOpr, b))
-    //
-    //    val l2 = List(1)
-    //    val fold2 = l2.take(l2.size - 1).foldRight(l2.last.toString)((a: Int, b: String) => "(" + a + ", " + b + ")")
-    //    println("DONE")
-
     val file = scala.io.Source.fromFile("programs/listcomprehensions2.iml")
     val imlcode = file.mkString
     file.close()
---
+++ IMLCompiler/src/ch/fhnw/imlcompiler/transforming/ResultTest.java
@@ -0,0 +1,106 @@
+package ch.fhnw.imlcompiler.transforming;
+
+public class ResultTest {
+
+	// global
+	static Wrapper _intValue = new Wrapper(); // intValue:int;
+	static Wrapper _list = new Wrapper(); // list:[int];
+	static Wrapper _nestedList = new Wrapper(); // list:[int];
+
+	// TODO what if nested :: operations are used (does it still work)?
+	// used for :: operations
+	static Object[] tmp;
+
+	public static void addThree(Wrapper _value /* in ref int32 */) { // proc
+		_value.value = (int) _value.value + 3;
+	}
+
+	// if by value direct access, else via Wrapper
+	public static void addThree2(int value /* in copy int32 */) { // proc
+		value = value + 3;
+	}
+
+	/*
+	 * list init := [1,2,3,4];
+	 * list := 0 :: list;
+	 * debugout list;
+	 * 
+	 * nestedList init := [[1,2],[3,4]];
+	 * nestedList := [-2,-1,0] :: nestedList;
+	 * debugout nestedList;
+	 * 
+	 * intValue init := head list;
+	 * debugout intValue;
+	 * 
+	 * list := head nestedList;
+	 * debugout list;
+	 * 
+	 * list := tail list;
+	 */
+	public static void main(String[] args) {
+		// list init := [1,2,3,4];
+		_list.value = new Object[] { 1, 2, 3, 4 };
+
+		// list := 0 :: list;
+		tmp = (Object[]) _list.value;
+		_list.value = new Object[((Object[]) _list.value).length + 1];
+		System.arraycopy(tmp, 0, _list.value, 1, tmp.length);
+		((Object[]) _list.value)[0] = 0;
+
+		// debugout list;
+		printarr((Object[]) _list.value);
+
+		// nestedList init := [[1,2],[3,4]];
+		_nestedList.value = new Object[] { new Object[] { 1, 2 }, new Object[] { 3, 4 } };
+
+		// nestedList := [-2,-1,0] :: nestedList;
+		tmp = (Object[]) _nestedList.value;
+		_nestedList.value = new Object[((Object[]) _nestedList.value).length + 1];
+		System.arraycopy(tmp, 0, _nestedList.value, 1, tmp.length);
+		((Object[]) _nestedList.value)[0] = new Object[] { -2, -1, 0 };
+
+		// debugout nestedList;
+		printarr((Object[]) _nestedList.value);
+
+		// intValue init := head list;
+		_intValue.value = ((Object[]) _list.value)[0];
+		System.out.println(_intValue.value);
+
+		// list := head nestedList;
+		_list.value = ((Object[]) _nestedList.value)[0]; // possible due to immutability of lists
+
+		// debugout list;
+		printarr((Object[]) _list.value);
+
+		_list.value = new Object[((Object[]) _nestedList.value).length - 1];
+		System.arraycopy(_nestedList.value, 1, _list.value, 0, ((Object[]) _nestedList.value).length - 1);
+
+		// debugout list;
+		printarr((Object[]) _list.value);
+	}
+
+	private static void printarr(Object[] arr) {
+		printarr(arr, true);
+	}
+
+	private static void printarr(Object[] arr, boolean newline) {
+		System.out.print("[");
+		for (int i = 0; i < arr.length; ++i) {
+			Object val = arr[i];
+			if (val instanceof Object[]) {
+				printarr((Object[]) val, false);
+			} else {
+				System.out.print(val);
+			}
+
+			if (i < arr.length - 1)
+				System.out.print(", ");
+		}
+		System.out.print("]" + (newline ? "\n" : ""));
+	}
+
+	// by reference wrapper
+	private static class Wrapper {
+		public Object value;
+	}
+}
